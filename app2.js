de1c492bb003cbd5eac603b07254f80b63cdf3ee
Pedro Guimarães <contato@pedroguimaraes.dev>
2024-04-23 21:55:24 -0300

    ajustes

--- app2.js
+++ app2.js
@@ -14,7 +14,6 @@ async function run(){
         }
     })
     
-    // console.log(encodeURIComponent(message))
     await send(encodeURIComponent(message))
     
 }
@@ -24,21 +23,16 @@ async function getNews() {
   const page = await browser.newPage()
   await page.goto('https://google.com')
    
-  // preenche o campo de busca do google
   await page.type('input[name="q"]', 'corona virus')
 
-  // promise de aguardo de carregamento da página
   const waitForLoad = new Promise(resolve => page.on('load', () => resolve()))
 
-  // clica no botão de pesquisa do google
   await page.evaluate(() => {
       document.querySelector('input[value="Pesquisa Google"]').click()
   })
 
-  // promise de carregamento
   await waitForLoad
 
-  // carrega as últimas notícias num array
   const links = await page.evaluate(() => {
     const news = []
 
@@ -51,10 +45,6 @@ async function getNews() {
 
   return links
 
-//   await new Promise(resolve => setTimeout(resolve, 5000));
-//   await browser.close();
-}
-
 async function send(message) {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
@@ -63,14 +53,13 @@ async function send(message) {
 
     const waitForLoad = new Promise(resolve => page.on('load', () => resolve()))
 
-    // aguarda o elemento ser carregado
     await page.waitForSelector('span[title="Felipe Santos"]')
     await page.waitForSelector('span[title="Danilo"]')
     
 
     await page.evaluate(() => {
-         document.querySelector('span[title="Felipe Santos"]').click()
-         document.querySelector('span[title="Danilo"]').click()
+         document.querySelector('span[title=""]').click()
+         document.querySelector('span[title=""]').click()
     })
     
     await page.waitForSelector('._3hV1n')
@@ -79,21 +68,6 @@ async function send(message) {
         // send button
         document.querySelector('._3hV1n').click()
     })
-   
-    // await page.evaluate(() => {
-
-    //     const eventFire = (el, etype) => {
-    //         var evt = document.createEvent("MouseEvents");
-    //         evt.initMouseEvent(etype, true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null)
-    //         el.dispatchEvent(evt)
-    //     }
-
-    //     eventFire(document.querySelector('span[title="Nelma"'), 'mousedown')
-    // })
-
-    // await page.waitForSelector('._1Plpp')
-
-    // await page.type('._1Plpp', message)
   
     await new Promise(resolve => setTimeout(resolve, 5000));
     await browser.close();
